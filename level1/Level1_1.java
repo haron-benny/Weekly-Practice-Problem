e3a1ccf2aea737ca5b6fd9de4007e0bb75b0014b
haron-benny <hb2863@srmist.edu.in>
2026-03-13 00:42:29 +0530

    renamed files and classes

---
+++ level1/Level1_1.java
@@ -0,0 +1,11 @@
+// Write a Java program to print "Hello, Welcome to Java Programming".
+
+public class Level1_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Welcome to Java Programming!");
+
+    }
+
+}
---
+++ level1/Level1_2.java
@@ -0,0 +1,5 @@
+package level1;
+
+public class Level1_2 {
+    
+}
